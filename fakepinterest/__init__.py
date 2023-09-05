9e9dedb6025b29e94c3f6ee5d99fe56c981d7824
dev-julio <julio2909cesar@gmail.com>
2023-09-05 18:12:46 -0300

    edit no database

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,11 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
 app.config["SECRET_KEY"] = "c684a766eca617db56fdf2febb8ca51e"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
